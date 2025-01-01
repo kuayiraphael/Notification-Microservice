c5f4f36ffd3483d8ff2df86caa5518f94d1e20b0
Kuayi Raphael <tinacity111@gmail.com>
2025-01-01 12:12:18 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const print = console.log;
+const cors = require("cors");
+
+const notificationRoutes = require("./api/notifications");
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+
+const { CreateChannel, SubscribeMessage } = require("./utils");
+
+require("dotenv").config();
+app.use(express.urlencoded({ extended: true }));
+
+async function startApp() {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    print("Connected to Notifications DB");
+
+    const channel = await CreateChannel();
+
+    await notificationRoutes(app, channel);
+    app.listen(8004, () => {
+      console.log("Notifications is Listening to Port 8004");
+    });
+  } catch (err) {
+    console.log("Failed to start app:", err);
+  }
+}
+
+startApp();
---
+++ services/notification-service.js
@@ -0,0 +1,71 @@
+const NotificationRepository = require("../database/repository/notification-repository");
+const { FormatData } = require("../utils");
+const nodeMailer = require("nodemailer");
+const Notification = require("../database/models/Notification");
+require("dotenv").config();
+class NotificationService {
+  // constructor() {
+  //   this.repository = new NotificationRepository();
+  // }
+
+  async sendCheckoutEmail(recipientEmail, orderId, items, amount) {
+    let itemsBought = "";
+
+    try {
+      const transporter = nodeMailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: "tinacity111@gmail.com",
+          pass: process.env.APP_PASSWORD,
+        },
+      });
+      for (let i = 0; i < items.length; i++) {
+        itemsBought += `${items[i].product.name}, `;
+      }
+
+      const mail = await transporter.sendMail({
+        from: '"MultiVendorApplication" <tinacity111@gmail.com>',
+        to: recipientEmail,
+        subject: "Checkout created",
+        html: `<h3>Dear Sir/Madam, you have created a checkout foor ${amount} dollars. Your order id is ${orderId} for the items: ${itemsBought}. If this was not done by you contact our customer support. Thank you </h3>`,
+      });
+
+      console.log("Email sent successfully:", mail.messageId);
+    } catch (err) {
+      console.error("Error sending email:", err);
+    }
+    let emailContent = `<h3>Dear Sir/Madam , you have created a checkout foor ${amount} dollars. Your order id is ${orderId} for the items: ${itemsBought}. If this was not done by you contact our customer support</h3>`;
+
+    const notification = new Notification({
+      recepient: recipientEmail,
+      content: emailContent,
+    });
+    const newNotification = await notification.save();
+    console.log("SAVED NOTIFICATION", newNotification);
+  }
+
+  async SubscribeEvents(payload) {
+    payload = JSON.parse(payload);
+    console.log("PAYLOAD IN NOTIFICATION SERVICE");
+    console.log(payload);
+    const { event, data } = payload;
+    const { userEmail, order } = data;
+
+    console.log("ORDER ITEMS IN NOTIF SERVICE???????", order.items);
+    console.log(data);
+    switch (event) {
+      case "SEND_CHECKOUT_CONFIRMATION_MAIL":
+        this.sendCheckoutEmail(
+          userEmail,
+          order.orderId,
+          order.items,
+          order.amount
+        );
+        break;
+    }
+  }
+}
+
+module.exports = NotificationService;
